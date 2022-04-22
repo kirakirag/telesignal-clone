2df85889ae53a24714920a7b24bdb563287b02ff
kirakirashit <kiratechno@pm.me>
2022-04-23 01:19:50 +0400

    uploading stickers works, need to add persistence and clean up

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
+from ast import parse
+import json
+import logging
 import io
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-import json
-import logging
+from signalstickers_client import StickersClient
+from signalstickers_client.models import LocalStickerPack, Sticker
 
 
 # Configure logging
@@ -13,7 +16,10 @@ logger = logging.getLogger(__name__)
 
 try:
     with open("credentials.json", encoding="UTF-8") as f:
-        API_TOKEN = json.load(f)["api_token"]
+        parsed = json.load(f)
+        TG_API_TOKEN = parsed["api_token"]
+        SIGNAL_UID = parsed["signal_uid"]
+        SIGNAL_PASSWORD = parsed["signal_password"]
 except FileNotFoundError:
     logger.error("Credentials not found. Please create a credentials.json file")
 except KeyError:
@@ -21,17 +27,29 @@ except KeyError:
 
 
 # Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=TG_API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
 class States(StatesGroup):
-    """Class that implements bot states."""
+    """Bot states implementation"""
 
     new = State()
     waiting = State()
     done = State()
+
+
+class DownloadedSticker:
+    """Downloaded sticker object
+
+    Returns:
+        DownloadSticker: object
+    """
+
+    def __init__(self, emoji: str, image: io.BytesIO):
+        self.emoji = emoji
+        self.image = image
 
 
 @dp.message_handler(commands=["start"])
@@ -66,12 +84,29 @@ async def sticker_handler(message: types.Message, state: FSMContext) -> None:
         sticker_set = await bot.get_sticker_set(set_name)
         logger.info("Found sticker set, length: %s", len(sticker_set.stickers))
         sticker_list = await get_sticker_list(sticker_set)
-        link = await upload_stickers_to_signal(sticker_list)
+        link = await upload_stickers_to_signal(
+            pack_title=set_name, sticker_list=sticker_list
+        )
 
         await message.reply(link)
 
 
-async def upload_stickers_to_signal(sticker_list: list) -> str:
+def add_sticker(pack: LocalStickerPack, sticker: DownloadedSticker):
+    stick = Sticker()
+    stick.emoji = sticker.emoji
+    stick.image_data = sticker.image.read()
+    stick.id = pack.nb_stickers
+
+    logger.info("Adding sticker %s to local sticker pack", stick.id)
+    pack._addsticker(stick)
+
+
+async def upload_stickers_to_signal(
+    pack_title: str,
+    sticker_list: str,
+    pack_author: str = "SignalStickerBot",
+    cover: io.BytesIO = None,
+) -> str:
     """Upload list of stickers to Signal and return a link
 
     Args:
@@ -80,7 +115,36 @@ async def upload_stickers_to_signal(sticker_list: list) -> str:
     Returns:
         str: link to signal sticker set
     """
-    pass
+
+    pack = LocalStickerPack()
+
+    # Set here the pack title and author
+    pack.title = pack_title
+    pack.author = pack_author
+
+    # Add the stickers here, with their emoji
+    # Accepted format:
+    # - Non-animated webp
+    # - PNG
+    # - GIF <100kb for animated stickers
+    for sticker in sticker_list:
+        add_sticker(pack, sticker)
+    logger.info("Created a local sticker pack")
+
+    # Instantiate the client with your Signal crendentials
+    async with StickersClient(SIGNAL_UID, SIGNAL_PASSWORD) as client:
+        # Upload the pack
+        try:
+            logger.info("Uploading local sticker pack to Signal")
+            pack_id, pack_key = await client.upload_pack(pack)
+            link = (
+                f"https://signal.art/addstickers/#pack_id={pack_id}&pack_key={pack_key}"
+            )
+        except Exception as e:
+            logger.error("Couldn't upload sticker pack! Exception: %s", e)
+            link = "Error, couldn't upload sticker set."
+
+    return link
 
 
 async def get_sticker_list(sticker_set: types.sticker_set) -> list:
@@ -101,8 +165,8 @@ async def get_sticker_list(sticker_set: types.sticker_set) -> list:
         file = await bot.get_file(sticker_id)
         file_path = file.file_path
 
-        result: io.BytesIO = await bot.download_file(file_path)
-        sticker_list.append(result)
+        sticker_image: io.BytesIO = await bot.download_file(file_path)
+        sticker_list.append(DownloadedSticker(sticker.emoji, sticker_image))
     return sticker_list
 
 
